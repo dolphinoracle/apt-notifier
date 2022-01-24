efdf214879a2dbd362d4a4456c1c46fbf605debb
fehlix <feh.ler@t-online.de>
2022-01-24 19:31:32 +0100

    adjust lock check, min./max. update check interval

--- lib/modules/apt-notifier.py
+++ lib/modules/apt-notifier.py
@@ -264,8 +264,8 @@ def set_globals():
     check_for_updates_interval = int(check_for_updates_interval)
     if check_for_updates_interval < 15:
         check_for_updates_interval = 15
-    elif check_for_updates_interval > 600:
-        check_for_updates_interval = 600 
+    elif check_for_updates_interval > 21600:
+        check_for_updates_interval = 21600 
     
 
     if check_for_updates_force_counter is None:
@@ -408,17 +408,6 @@ def check_updates():
         return
 
     """
-    Don't bother checking for updates if processes for other package management tools
-    appear to be runninng. For unattended-upgrade, use '/usr/bin/unattended-upgrade'
-    to avoid getting a hit on /usr/share/unattended-upgrades/unattended-upgrade-shutdown
-    which appears to be started automatically when using systemd as init.
-    """
-    if apt_dpkg_is_locked():
-        Force_Check_Counter = check_for_updates_force_counter
-        Check_for_Updates_by_User = False
-        return
-
-    """
     Get a hash of files and directories we are watching
     """
     WatchedFilesAndDirsHashNow = get_stat_hash_of_watched_files_and_dirs()
@@ -443,6 +432,17 @@ def check_updates():
         hash_changed = True
     
     WatchedFilesAndDirsHashPrevious = WatchedFilesAndDirsHashNow
+
+    """
+    Don't bother checking for updates if processes for other package management tools
+    appear to be runninng. For unattended-upgrade, use '/usr/bin/unattended-upgrade'
+    to avoid getting a hit on /usr/share/unattended-upgrades/unattended-upgrade-shutdown
+    which appears to be started automatically when using systemd as init.
+    """
+    if apt_dpkg_is_locked():
+        Force_Check_Counter = check_for_updates_force_counter
+        Check_for_Updates_by_User = False
+        return
 
     if Check_for_Updates_by_User or hash_changed:
         short_run = False
@@ -1885,9 +1885,7 @@ def main():
 
     global check_for_updates_interval
 
-    # Timer.start(60000)
-    # Timer.start(int(check_for_updates_interval)*1000)
-    Timer.start(15000)
+    Timer.start(int(check_for_updates_interval)*1000)
     if AptNotify.isSessionRestored():
         sys.exit(1)
     sys.exit(AptNotify.exec_())
